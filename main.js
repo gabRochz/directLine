824aa6175391e5a3f3e83c86f03943acebdebedb
Gabriel Rocha <92760944+gabRochz@users.noreply.github.com>
2021-12-08 12:40:11 -0300

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-function bandeiras() {
-  var select = document.getElementById('paises')
-  var option = select.options[select.selectedIndex]
-  var res = document.getElementById('nacionalidade')
-
-  document.getElementById('bandeira').src = option.value
-}
-bandeiras()
